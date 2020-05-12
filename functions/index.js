cd308dddc488187b1673f57a16407368e80f1546
Naveen <naveendevinda163@gmail.com>
2020-05-12 22:01:07 +0530

    Use express

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,11 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require('express');
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.getScreams = functions.https.onRequest((req, res) => {
-     admin.firestore().collection('screams').get()
+app.get('/screams', (req, res) => {
+    admin.firestore().collection('screams').get()
         .then(data => {
             let screams = [];
             data.forEach (doc => {
@@ -22,7 +18,7 @@ exports.getScreams = functions.https.onRequest((req, res) => {
         .catch(err => console.error(err));
 });
 
-exports.createScream  = functions.https.onRequest((req, res) => {
+app.post('/scream', (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
@@ -38,3 +34,5 @@ exports.createScream  = functions.https.onRequest((req, res) => {
             console.error(err);
         });
 });
+
+exports.api = functions.https.onRequest(app);
