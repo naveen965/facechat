d1a22a7818cd06fa9c2aac68a84379716f135207
Naveen <naveendevinda163@gmail.com>
2020-05-16 21:54:23 +0530

    edited

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,14 @@ const functions = require('firebase-functions');
 
 const app = require('express')();
 
-const FBAuth = require('./util/fbAuth');
+const FBauth = require('./util/fbAuth');
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
 const { signUp, login } = require('./handlers/users');
 
 app.get('/screams', getAllScreams);
-
 app.post('/scream', FBauth, postOneScream);
-
 app.post('/signup', signUp);
-
 app.post('/login', login);
 
 exports.api = functions.region('us-central1').https.onRequest(app);
