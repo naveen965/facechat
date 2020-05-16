fcf2c396b44ec7d74406ed69a7fd1abddb76da34
Naveen <naveendevinda163@gmail.com>
2020-05-16 20:25:07 +0530

    Create new files and folders

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,17 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+
 const app = require('express')();
 
-admin.initializeApp();
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCxwIMJVZw9vlVSiBWroW2ZSu_lqemoLFg",
-    authDomain: "socialape-48ee7.firebaseapp.com",
-    databaseURL: "https://socialape-48ee7.firebaseio.com",
-    projectId: "socialape-48ee7",
-    storageBucket: "socialape-48ee7.appspot.com",
-    messagingSenderId: "527780846820",
-    appId: "1:527780846820:web:6868a14b4246e9610ca0f1",
-    measurementId: "G-05BR1KS82F"
-  };
+const { getAllScreams } = require('./handlers/screams');
+const { signUp, login } = require('./handlers/users');
 
 
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
 
-const db = admin.firestore();
+app.get('/screams', getAllScreams);
 
-app.get('/screams', (req, res) => {
-    db.collection('screams').orderBy('createdAt', 'desc').get()
-        .then(data => {
-            let screams = [];
-            data.forEach (doc => {
-                screams.push({
-                    screamId: doc.id,
-                    body: doc.data().body,
-                    userHandle: doc.data().userHandle,
-                    createdAt: doc.data().createdAt
-                });
-            });
-            return res.status(200).json(screams);
-        })
-        .catch(err => console.error(err));
-});
+app.post('/scream', FBauth, postOneScream);
 
 const FBauth = (req, res, next) => {
     let idToken;
@@ -65,23 +40,6 @@ const FBauth = (req, res, next) => {
         });
 }
 
-app.post('/scream', FBauth, (req, res) => {
-    const newScream = {
-        body: req.body.body,
-        userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
-    };
-
-    db.collection('screams').add(newScream)
-        .then((doc) => {
-            res.status(200).json({ message: `document ${doc.id} created successfully` });
-        })
-        .catch((err) => {
-            res.status(500).json({ error: 'Something went wrong' });
-            console.error(err);
-        });
-});
-
 const isEmail = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email.match(regEx)) return true;
@@ -93,96 +51,8 @@ const isEmpty = (String) => {
     else return false;
 }
 
-app.post('/signup', (req, res) => {
-    const newUser = {
-        email: req.body.email,
-        password: req.body.password,
-        confiremPassword: req.body.confiremPassword,
-        handle: req.body.handle,
-    };
+app.post('/signup', signUp);
 
-    let errors = {};
-
-    if (isEmpty(newUser.email)) {
-        errors.email = 'Email must not be empty';
-    } else if (!isEmail(newUser.email)) {
-        errors.email = 'Must be a valid email address';
-    }
-
-    if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
-    if (newUser.password !== newUser.confiremPassword) errors.confiremPassword = 'Password must be matched';
-    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
-
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-    let token, userId;
-
-    db.doc(`/users/${newUser.handle}`)
-        .get()
-        .then(doc => {
-            if (doc.exists) {
-                return res.status(400).json({ handle: 'This handle is already taken' });
-            } else {
-                return firebase
-                    .auth()
-                    .createUserWithEmailAndPassword(newUser.email, newUser.password);
-            }
-        })
-        .then(data => {
-            userId = data.user.uid;
-            return data.user.getIdToken();
-        })
-        .then(idToken => {
-            token = idToken;
-            const userCredentials = {
-                handle: newUser.handle,
-                email: newUser.email,
-                createdAt: new Date().toISOString(),
-                userId
-            };
-            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-        })
-        .then(() => {
-            return res.status(201).json({ token });
-        })
-        .catch((err) => {
-            console.error(err);
-            if (err.code === 'auth/email-already-in-use') {
-                return res.status(400).json({ email: 'Email is already use' });
-            } else {
-                res.status(500).json({ error: err.code });
-            }
-        });
-});
-
-app.post('/login', (req, res) => {
-    const user = {
-        email: req.body.email,
-        password: req.body.password
-    };
-
-    let errors = {};
-
-    if (isEmpty(user.email)) errors.email = 'Must not be empty';
-    if (isEmpty(user.password)) errors.password = 'Must not be empty';
-
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-        .then(data => {
-            return data.user.getIdToken();
-        })
-        .then(token => {
-            return res.json({ token });
-        })
-        .catch(err => {
-            console.error(err);
-            if (err.code === 'auth/wrong-password') {
-                return res.status(403).json({ general: 'Wrong credentials, please try again' });
-            } else {
-                res.status(500).json({ error: err.code });
-            }
-        });
-});
+app.post('/login', login);
 
 exports.api = functions.region('us-central1').https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCxwIMJVZw9vlVSiBWroW2ZSu_lqemoLFg",
+    authDomain: "socialape-48ee7.firebaseapp.com",
+    databaseURL: "https://socialape-48ee7.firebaseio.com",
+    projectId: "socialape-48ee7",
+    storageBucket: "socialape-48ee7.appspot.com",
+    messagingSenderId: "527780846820",
+    appId: "1:527780846820:web:6868a14b4246e9610ca0f1",
+    measurementId: "G-05BR1KS82F"
+};
