ccc224bfffb157e5ad5956ce2532885347dba7d2
Naveen <naveendevinda163@gmail.com>
2020-05-16 20:32:30 +0530

    Create new file

--- functions/index.js
+++ functions/index.js
@@ -13,32 +13,9 @@ app.get('/screams', getAllScreams);
 
 app.post('/scream', FBauth, postOneScream);
 
-const FBauth = (req, res, next) => {
-    let idToken;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-        idToken = req.headers.authorization.split('Bearer ')[1];
-    } else {
-        console.error('No token found');
-        return res.status(403).json({ error: 'Unauthorized' });
-    }
-    admin.auth().verifyIdToken(idToken)
-        .then(decodedToken => {
-            req.user = decodedToken;
-            console.log(decodedToken);
-            return db.collection('users')
-                .where('userId', '==', req.user.uid)
-                .limit(1)
-                .get();
-        })
-        .then(data => {
-            req.user.handle = data.docs[0].data().handle;
-            return next();
-        })
-        .catch(err => {
-            console.error('Error while verifying token ', err);
-            return res.status(403).json(err);
-        });
-}
+app.post('/signup', signUp);
+
+app.post('/login', login);
 
 const isEmail = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -50,9 +27,5 @@ const isEmpty = (String) => {
     if (String.trim() === '') return true;
     else return false;
 }
-
-app.post('/signup', signUp);
-
-app.post('/login', login);
 
 exports.api = functions.region('us-central1').https.onRequest(app);
