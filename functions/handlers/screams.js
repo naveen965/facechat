723b42e902b0327284a98f85ac8d9008bd894499
Naveen <naveendevinda163@gmail.com>
2020-05-20 15:44:23 +0530

    edit

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -29,12 +29,18 @@ exports.postOneScream = (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
+        userImage: req.user.imageUrl,
+        createdAt: new Date().toISOString(),
+        likeCount: 0,
+        commentCount: 0
     };
 
-    db.collection('screams').add(newScream)
+    db.collection('screams')
+        .add(newScream)
         .then((doc) => {
-            res.status(200).json({ message: `document ${doc.id} created successfully` });
+            const resScream = newScream;
+            resScream.screamId = doc.id;
+            res.json(newScream);
         })
         .catch((err) => {
             res.status(500).json({ error: 'Something went wrong' });
@@ -98,3 +104,11 @@ exports.commentOnScream = (req, res) => {
             res.status(500).json({ error: 'Something went wrong' });
         });
 };
+
+exports.likeScream = (req, res) => {
+
+}
+
+exports.unlikeScream = (req, res) => {
+    
+}
