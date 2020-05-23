6db416380ef2459105b66c001d528942ad5107bd
Naveen <naveendevinda163@gmail.com>
2020-05-23 22:56:32 +0530

    Fix code

--- functions/index.js
+++ functions/index.js
@@ -112,11 +112,11 @@ exports.onUserImageChange = functions.region('us-central1').firestore.document('
                     })
                     return batch.commit();
                 });
-        }
+        } else return true;
     })
 
 exports.onScreamDelete = functions.region('us-central1').firestore.document('/screams/{screamId}')
-    onDelete((snapshot, context) => {
+    .onDelete((snapshot, context) => {
         const screamId = context.params.screamId;
         const batch = db.batch();
         return db.collection('comments').where('screamId', '==', screamId).get()
@@ -124,12 +124,21 @@ exports.onScreamDelete = functions.region('us-central1').firestore.document('/sc
                 data.forEach(doc => {
                     batch.delete(db.doc(`/comments/${doc.id}`));
                 })
-                return db.collection('likes').where('screamId', '==', screamId);
+                return db.collection('likes').where('screamId', '==', screamId).get();
             })
             .then(data => {
                 data.forEach(doc => {
                     batch.delete(db.doc(`/likes/${doc.id}`));
                 })
-                return db.collection('notifications').where('screamId', '==', screamId);
+                return db.collection('notifications').where('screamId', '==', screamId).get();
             })
-    })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/notifications/${doc.id}`));
+                })
+                return batch.commit();
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    });
