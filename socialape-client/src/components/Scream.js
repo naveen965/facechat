4933af2f778bf91c30d397d10ab15f926c44ead3
Naveen <naveendevinda163@gmail.com>
2020-05-25 23:04:42 +0530

    Edited

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -8,7 +8,15 @@ import { Link } from 'react-router-dom';
 
 const styles = {
     card: {
-        display: 'flex'
+        display: 'flex',
+        marginBottom: 20,
+    },
+    image: {
+        minWidth: 200
+    },
+    content: {
+        padding: 25,
+        objectFit: 'cover'
     }
 }
 
@@ -27,10 +35,10 @@ class Scream extends Component {
             }
         } = this.props;
         return (
-            <Card>
-                <CardMedia image={userImage} title="Profile image"/>
-                <CardContent>
-                    <Typography variant="h5" component={Link} to={`/users/${userHandle}`}>{userHandle}</Typography>
+            <Card className={classes.card}>
+                <CardMedia image={userImage} title="Profile image" className={classes.image}/>
+                <CardContent class={classes.content}>
+                    <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
                     <Typography variant="body1">{body}</Typography>
                     <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
                 </CardContent>
