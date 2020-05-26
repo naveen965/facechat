1f27ebe8edebe6b53ebfc7a0db52809ef4aa37d4
Naveen <naveendevinda163@gmail.com>
2020-05-26 11:48:50 +0530

    Edited

--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -2,21 +2,102 @@ import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
+import Appicon from '../images/User.png';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 const styles = {
     form: {
-        textAlign: 'center'
+        textAlign: 'center',
+    },
+    image: {
+        margin: '20px auto 20px auto'
+    },
+    pageTitle: {
+        margin: '10px auto 10px auto'
+    },
+    button: {
+        width: '100%',
+        marginTop: 20
+    },
+    TextField: {
+        margin: '10px auto 10px auto'
     }
-}
+};
 
 class login extends Component {
+    constructor () {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {}
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            loading: true
+        });
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        axios.post('/login', userData)
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    loading: false
+                });
+                this.props.history.push('/');
+            })
+            .catch(err => {
+                this.setState({
+                    errors: err.reponse.data,
+                    loading: false
+                })
+            })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
     render() {
         const { classes } = this.props;
+        const { errors, loading } = this.state;
         return (
-            <Grid container className={classes.from}>
+            <Grid container className={classes.form}>
                 <Grid item sm/>
                 <Grid item sm>
-                    <p>Yoooo</p>
+                    <img src={Appicon} alt="user" className={classes.image}/>
+                    <Typography variant="h2" className={classes.pageTitle}>
+                        Login
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField id="email" name="email" type="email" label="Email"
+                            className={classes.TextField}
+                            helperText={errors.email}
+                            error={errors.email ? true : false}
+                            value={this.state.email}
+                            onChange={this.handleChange} fullWidth/>
+                        <TextField id="password" name="password" type="password" label="Password"
+                            className={classes.TextField}
+                            helperText={errors.password}
+                            error={errors.password ? true : false}
+                            value={this.state.password}
+                            onChange={this.handleChange} fullWidth/>
+                        <Button type="submit" variant="contained" color="primary"
+                            className={classes.button}>
+                                Login
+                            </Button>
+                    </form>
                 </Grid>
                 <Grid item sm/>
             </Grid>
