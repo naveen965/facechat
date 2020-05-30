ea04b9da193f4844aa9f7bcbaa240e4a78451352
Naveen <naveendevinda163@gmail.com>
2020-05-30 12:23:58 +0530

    Edit

--- socialape-client/src/redux/actions/userAction.js
+++ socialape-client/src/redux/actions/userAction.js
@@ -21,6 +21,23 @@ export const loginUser = (userData, history) => (dispatch) => {
         });
 }
 
+export const signupUser = (newUserData, history) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+    axios
+        .post('/signup', newUserData)
+        .then(res => {
+            dispatch(getUserData());
+            dispatch({ type: CLEAR_ERRORS });
+            history.push('/');
+        })
+        .catch(err => {
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data
+            })
+        });
+}
+
 export const getUserData = () => (dispatch) => {
     axios.get('/user')
         .then(res => {
@@ -32,4 +49,10 @@ export const getUserData = () => (dispatch) => {
         .catch(err => {
             console.log(err);
         });
+}
+
+const setAuthorizationHeader = (token) => {
+    const FBIdToken = `Bearer ${res.data.token}`;
+    localStorage.setItem('FBIdToken', FBIdToken);
+    axios.defaults.headers.common['Authorization'] = FBIdToken;
 }
