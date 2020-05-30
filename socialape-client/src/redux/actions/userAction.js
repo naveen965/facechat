3ba1876739fd17dd2829acb40fe2e742c3bc0179
Naveen <naveendevinda163@gmail.com>
2020-05-30 12:37:14 +0530

    edit userAction.js

--- socialape-client/src/redux/actions/userAction.js
+++ socialape-client/src/redux/actions/userAction.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types';
 import axios from 'axios';
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -6,9 +6,7 @@ export const loginUser = (userData, history) => (dispatch) => {
     axios
         .post('/login', userData)
         .then(res => {
-            const FBIdToken = `Bearer ${res.data.token}`;
-            localStorage.setItem('FBIdToken', FBIdToken);
-            axios.defaults.headers.common['Authorization'] = FBIdToken;
+            setAuthorizationHeader(res.data.token);
             dispatch(getUserData());
             dispatch({ type: CLEAR_ERRORS });
             history.push('/');
@@ -26,6 +24,7 @@ export const signupUser = (newUserData, history) => (dispatch) => {
     axios
         .post('/signup', newUserData)
         .then(res => {
+            setAuthorizationHeader(res.data.token);
             dispatch(getUserData());
             dispatch({ type: CLEAR_ERRORS });
             history.push('/');
@@ -36,6 +35,12 @@ export const signupUser = (newUserData, history) => (dispatch) => {
                 payload: err.response.data
             })
         });
+}
+
+export const logoutUser = () => (dispatch) => {
+    localStorage.removeItem('FBIdToken');
+    delete axios.defaults.headers.common['Authorization'];
+    dispatch({ type: SET_UNAUTHENTICATED });
 }
 
 export const getUserData = () => (dispatch) => {
