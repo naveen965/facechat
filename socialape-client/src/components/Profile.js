783df837362e5d88d948cc1cd223d383c1e07546
Naveen <naveendevinda163@gmail.com>
2020-05-31 20:13:52 +0530

    Create new file

---
+++ socialape-client/src/components/Profile.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Profile));
