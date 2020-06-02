7dd63ec2d3993cf08d1c28f2817f9a17167d4ae4
Naveen <naveendevinda163@gmail.com>
2020-06-02 09:20:35 +0530

    Image Upload

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -64,6 +64,9 @@ const styles = (theme) => ({
 class Profile extends Component {
     handleImageChange = (event) => {
         const image = event.target.files[0];
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        this.props.uploadImage(formData);
     };
     handleEditPicture = () => {
         const fileInput = document.getElementById('imageInput');
