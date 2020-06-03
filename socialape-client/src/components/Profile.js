f637643f8fbe1e2c37996b1b340be04bc17544e3
Naveen <naveendevinda163@gmail.com>
2020-06-03 21:25:00 +0530

    Create EditDetails.js

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -14,6 +14,7 @@ import dayjs from 'dayjs';
 import EditIcon from '@material-ui/icons/Edit';
 import { logoutUser, uploadImage } from '../redux/actions/userAction';
 import { KeyboardReturn } from '@material-ui/icons';
+import EditDetails from '../components/EditDetails';
 
 const styles = (theme) => ({
     paper: {
