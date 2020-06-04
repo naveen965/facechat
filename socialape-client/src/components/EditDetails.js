79c70ebb45d7b1b6c92595cce1688a795aa38ccc
Naveen <naveendevinda163@gmail.com>
2020-06-04 22:35:35 +0530

    Edited

--- socialape-client/src/components/EditDetails.js
+++ socialape-client/src/components/EditDetails.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { connect } from 'react-redux';
 import { editUserDetails } from '../redux/actions/userAction';
-import { Tooltip, IconButton } from '@material-ui/core';
+import { 
+    Tooltip,
+    IconButton,
+    Button,
+    TextField,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
+} from '@material-ui/core';
+import EditIcon from '@material-ui/iconsEdit';
 
 const styles = (theme) => ({
     ...theme
@@ -16,18 +27,89 @@ class EditDetails extends Component {
         location: '',
         open: false
     };
+    handleOpen = () => {
+        this.setState({ open: true });
+        this.mapUserDetailsToState(this.props.credentials);
+    };
+    handleClose = () => {
+        this.setState({ open: false });
+    }
     componentDidMount() {
         const { credentials } = this.props;
+        this.mapUserDetailsToState(credentials);
+    };
+    mapUserDetailsToState = (credentials) => {
         this.setState({
             bio: credentials.bio ? credentials.bio : '',
             website: credentials.website ? credentials.website : '',
             location: credentials.location ? credentials.location : '',
-        })
-    }
+        });
+    };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
     render() {
+            const { classes } = this.props;
         return (
             <Fragment>
-                <Tooltip title="Edit details" placement="top"></Tooltip>
+                <Tooltip title="Edit details" placement="top">
+                    <IconButton onClick={this.handleOpen} className={classes.button}>
+                        <EditIcon color="primary"/>
+                    </IconButton>
+                </Tooltip>
+                <Dialog
+                open={this.state.open}
+                onClose={this.handleClose}
+                fullWidth
+                maxWidth="sm">
+                    <DialogTitle>Edit your details</DialogTitle>
+                    <DialogContent>
+                        <form>
+                            <TextField
+                                name="bio"
+                                type="text"
+                                label="Bio"
+                                multiline
+                                rows="3"
+                                placeholder="A short bio about yourself"
+                                className={classes.textField}
+                                value={this.state.bio}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                            <TextField
+                                name="website"
+                                type="text"
+                                label="Website"
+                                placeholder="Your personal/professional website"
+                                className={classes.textField}
+                                value={this.state.website}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                            <TextField
+                                name="location"
+                                type="text"
+                                label="Location"
+                                placeholder="Where you live"
+                                className={classes.textField}
+                                value={this.state.location}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                        </form>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.handleSubmit} color="primary">
+                            Save
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </Fragment>
         )
     }
