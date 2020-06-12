1a82f555fd8197a1b6260840331fa476cc26d8f9
Naveen <naveendevinda163@gmail.com>
2020-06-12 22:29:24 +0530

    Edit PostScream.js

--- socialape-client/src/components/PostScream.js
+++ socialape-client/src/components/PostScream.js
@@ -9,14 +9,15 @@ import {
     DialogTitle,
     CircularProgress
 } from '@material-ui/core';
-import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from '@material-ui/icons/Add';
+import CloseIcon from '@material-ui/icons/Close';
 import MyButton from '../util/MyButton';
 import { connect } from 'react-redux';
 import { postScream } from '../redux/actions/dataAction';
 
-const styles = {
-
-}
+const styles = (theme) => ({
+    ...theme
+})
 
 class PostScream extends Component {
     state = {
@@ -24,6 +25,52 @@ class PostScream extends Component {
         body: '',
         errors: {}
     };
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+    handleOpen = () => {
+        this.setState({ open: false });
+    };
+    render() {
+        const { errors } = this.state;
+        const { classes, UI: { loading } } = this.props;
+        return (
+            <Fragment>
+                <MyButton onClick={this.handleOpen} tip="Post a Scream!">
+                    <AddIcon color="primary"/>
+                </MyButton>
+                <Dialog open={this.state.open} onClose={this.handleClose} fullWidth maxWidth="sm">
+                    <MyButton tip="Close" onClick={this.handleClose} tipClassName={classes.closeButton}>
+                        <CloseIcon/>
+                    </MyButton>
+                    <DialogTitle>Post a new Scream</DialogTitle>
+                    <DialogContent>
+                        <form onSubmit={this.handleSubmit}>
+                            <TextField
+                                name="body"
+                                type="text"
+                                label="SCREAM!!"
+                                multiline
+                                rows="3"
+                                placeholder="Scream at your fellow apes"
+                                error={errors.body ? true : false}
+                                helperText={errors.body}
+                                className={classes.textField}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                            <Button type="submit" variant="contained" color="primary" className={classes.submitButton} disabled={loading}>
+                                Submit
+                                {loading && (
+                                    <CircularProgress size={30} className={classes.progressSpinner}/>
+                                )}
+                            </Button>
+                        </form>
+                    </DialogContent>
+                </Dialog>
+            </Fragment>
+        )
+    }
 }
 
 PostScream.propTypes = {
