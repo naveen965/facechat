482be33d220af877ee1a283be3455e882d3ca147
Naveen <naveendevinda163@gmail.com>
2020-06-16 21:31:28 +0530

    change

--- socialape-client/src/components/scream/Scream.js
+++ socialape-client/src/components/scream/Scream.js
@@ -66,7 +66,7 @@ class Scream extends Component {
                         <ChatIcon color="primary"/>
                     </MyButton>
                     <span>{commentCount} Comments</span>
-                    <ScreamDialog screamId={screamId} userHandle={userHandle}/>
+                    <ScreamDialog screamId={screamId} userHandle={userHandle} openDialog={this.props.openDialog}/>
                 </CardContent>
             </Card>
         );
--- socialape-client/src/pages/user.js
+++ socialape-client/src/pages/user.js
@@ -14,28 +14,38 @@ const styles = ({
 
 class user extends Component {
     state = {
-        profile: null
-    }
+        profile: null,
+        screamIdParam: null
+    };
     componentDidMount () {
         const handle = this.props.match.params.handle;
+        const screamId = this.props.match.params.screamId;
+        if (screamId) this.setState({ screamIdParam: screamId });
         this.props.getUserData(handle);
         axios.get(`/user/${handle}`)
             .then(res => {
                 this.setState({
                     profile: res.data.user
-                })
+                });
             })
             .catch(err => console.log(err));
-    }
+    };
     render() {
         const { screams, loading } = this.props.data;
+        const { screamIdParam } = this.state;
         const screamsMarkup = loading ? (
             <p>Loading data...</p>
         ) : screams === null ? (
             <p>No screams from this user</p>
-        ) : (
+        ) : !screamIdParam ? (
             screams.map(scream => <Scream key={scream.screamId} scream={scream}/>)
-        )
+        ) : (
+            screams.map(scream => {
+                if (scream.screamId !== screamIdParam)
+                    return <Scream key={scream.screamId} scream={scream}/>
+                else return <Scream key={scream.screamId} scream={scream} openDialog/>
+            })
+        );
         return (
             <Grid container spacing={10}>
                 <Grid item sm={8} xs={12}>
