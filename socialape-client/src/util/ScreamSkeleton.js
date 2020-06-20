f2dad0fd0da71e5e6ee78baffc1ec81219997f7c
Naveen <naveendevinda163@gmail.com>
2020-06-20 11:47:47 +0530

    create ProfileSkeleton.js

--- socialape-client/src/util/ScreamSkeleton.js
+++ socialape-client/src/util/ScreamSkeleton.js
@@ -8,15 +8,48 @@ import {
     CardContent
 } from '@material-ui/core';
 
-const styles = ({
+const styles = (theme) => ({
     card: {
         display: 'flex',
         marginBottom: 20
+    },
+    cardContent: {
+        width: '100%',
+        flexDirection: 'column',
+        padding: 25
+    },
+    cover: {
+        minWidth: 200,
+        objectFit: 'cover'
+    },
+    handle: {
+        width: 60,
+        height: 18,
+        backgroundColor: theme.palette.primary.main,
+        marginBottom: 7
+    },
+    date: {
+        height: 14,
+        width: 100,
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        marginBottom: 10
+    },
+    fullLine: {
+        height: 15,
+        width: '90%',
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+        marginBottom: 10
+    },
+    halfLine: {
+        height: 15,
+        width: '50%',
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+        marginBottom: 10
     }
 })
 
 const ScreamSkeleton = (props) => {
-    const { classes } = this.props;
+    const { classes } = props;
     const content = Array.from({ length: 5 }).map((item, index) => (
         <Card className={classes.card} key={index}>
             <CardMedia className={classes.cover} image={NoImg}/>
