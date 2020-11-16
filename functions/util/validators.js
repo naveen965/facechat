e155c6abfb13274b41d55208fe749138765891d0
naveen965 <39030430+naveen965@users.noreply.github.com>
2020-11-16 19:30:03 +0530

    Edited

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -45,7 +45,7 @@ exports.reduceUserDetails = (data) => {
 
     if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
     if (!isEmpty(data.website.trim())) {
-        if(data.website.trim().substring(0, 4) !== 'http') {
+        if (data.website.trim().substring(0, 4) !== 'http') {
             userDetails.website = `http://${data.website.trim()}`;
         } else userDetails.website = data.website;
     }
